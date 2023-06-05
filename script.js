53e835d5c38027c6774d81f456d3163affed107b
FabricioPorcelli <fabricioporcelli@gmail.com>
2023-06-05 17:34:49 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showAlert () {
+    alert("Hola, esta es una alerta desde Javascript!");
+}
+
+document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+        document.querySelector(".nav-links").
+                    classList.toggle("nav-links-responsive")});
